0d6dcaa5973b986d5ba6d23aba59e81d5720a821
mrparkersson <amoparkerprince78@gmail.com>
2022-06-23 12:13:42 +0000

    Add lesson service and create lesson methods

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,8 +1,12 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Lesson } from './lesson.entity';
+import { LessonService } from './lesson.service';
 import { LessonType } from './lesson.type';
 
 @Resolver((of) => LessonType)
 export class LessonResolver {
+  constructor(private lessonService: LessonService) {}
+
   @Query((returns) => LessonType)
   lesson() {
     return {
@@ -11,5 +15,14 @@ export class LessonResolver {
       startDate: new Date().toISOString(),
       endDate: new Date().toISOString(),
     };
+  }
+
+  @Mutation((returns) => LessonType)
+  async createLesson(
+    @Args('name') name: string,
+    @Args('startDate') startDate: string,
+    @Args('endDate') endDate: string,
+  ): Promise<Lesson> {
+    return this.lessonService.createLesson(name, startDate, endDate);
   }
 }
